ef3a4694652afcc0d4b388cf7fb180fe5da213aa
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-10-09 20:15:59 +0100

    App update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Features from './components/Features/Features';
 
 function App() {
   return (
     <div className="container">
       <Navbar/>
+      <Hero/>
+      <Features/>
     </div>
   );
 }
