4681f76becdf16c99fc9e2e8fb0145533410013a
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-10-10 11:04:49 +0100

    App update: How it works section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Features from './components/Features/Features';
+import HowItWorks from './components/HowItWorks/HowItWorks';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Features/>
+      <HowItWorks/>
     </div>
   );
 }
