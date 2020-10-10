2af72b850a80ec52213d7b656768799fac409702
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-10-10 16:13:47 +0100

    App update: FormSection section

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Hero from './components/Hero/Hero';
 import Features from './components/Features/Features';
 import HowItWorks from './components/HowItWorks/HowItWorks';
 import Availability from './components/Availability/Availability';
+import FormSection from './components/FormSection/FormSection';
 
 function App() {
   return (
-    <div className="container">
-      <Navbar/>
-      <Hero/>
-      <Features/>
-      <HowItWorks/>
-      <Availability/>
+    <div>
+      <div className="container">
+        <Navbar/>
+        <Hero/>
+        <Features/>
+        <HowItWorks/>
+        <Availability/>
+      </div>
+      <FormSection/>
     </div>
+
   );
 }
 
